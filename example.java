74982c3dad2a885193163587c842b98615ead8b5
Monisha287 <vmoni5557@gmail.com>
2025-09-17 23:51:44 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        System.out.println("Length of the string: " + input.length());
+    }
+}
