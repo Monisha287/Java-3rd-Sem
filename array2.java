3f59316c9a7ca1edfc42c3e2ec09b17e28f3d431
Monisha287 <vmoni5557@gmail.com>
2025-09-17 23:47:32 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,20 @@
+public class array2 {
+    public static void main (String[] args){
+        int n[] = new int[10];
+        n[0]=2;
+        n[1]=5;
+        n[2]=9;
+        n[3]=10;
+        n[4]=11;
+        n[5]=15;
+        n[6]=14;
+        n[7]=55;
+        n[8]=6;
+        n[9]=45;
+        System.out.println(n[6]);
+
+
+    }
+
+}
+
