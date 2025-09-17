f2a51a2c636235296f7dcd3901d68317e71b9d48
Monisha287 <vmoni5557@gmail.com>
2025-09-18 00:00:00 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,12 @@
+public class string {
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using a new keyword
+        String str = new String("Geeks");
+
+        System.out.println(str);
+    }
+}
